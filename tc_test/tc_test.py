684bda4754fcfd4f3e59fe651ba4fec4dd56dc93
yigitacar <yigit_flash3@live.com>
2024-12-05 14:31:59 +0300

    trace printing adjusted

--- tc_test/tc_test.py
+++ tc_test/tc_test.py
@@ -17,26 +17,5 @@ fd = f.sock
 sock = socket.fromfd(fd, socket.PF_PACKET, socket.SOCK_RAW, socket.IPPROTO_IP)
 sock.setblocking(True)
 
-# ipr = IPRoute()
-# links = ipr.link_lookup(ifname=interface)
-# idx = links[0]
-
-# try:
-#     ipr.tc("add", "ingress", idx, "ffff:")
-# except:
-#     print("qdisc ingress already exists")
-#
-# fi = b.load_func("tc_pingpong", BPF.SCHED_CLS)
-#
-# ipr.tc("add-filter", "bpf", idx, ":1", fd=fi.fd,
-#         name=fi.name, parent="ffff:", action="ok", classid=1, da=True)
-
-# Remove with sudo tc qdisc del dev docker0 parent ffff:
-# (or make clean)
-
-# Read data from socket filter
-while True:
-  packet_str = os.read(fd, 4096)
-  print("Userspace got data: %x", packet_str)
-
+b.trace_print()
 
