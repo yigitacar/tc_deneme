c8de594366cd6185c2e2148716c61653a058cd99
yigitacar <yigit_flash3@live.com>
2024-12-06 10:43:41 +0300

    printing available interfaces attempt

--- tc_test/tc_test.py
+++ tc_test/tc_test.py
@@ -6,21 +6,35 @@ from time import sleep
 from pyroute2 import IPRoute
 
 b = BPF(src_file="tc_test.c")
+
 interface = "ens4"
-
+## get available interfaces
 ipr = IPRoute()
-links = ipr.link_lookup(ifname=interface)
-idx = links[0]
+links = ipr.get_links()
+available_interfaces = []
 
-try:
-    ipr.tc("add", "egress", idx, ":1")
-except:
-    print("qdisc ingress already exists")
+for iface in available_interfaces:
+    ifname = iface.get_attr("IFLA_IFNAME")
+    if ifname and ifname != "lo":
+        available_interfaces.append(ifname)
 
-fi = b.load_func("tc_ack", BPF.SCHED_CLS)
+print("Available interfaces:", available_interfaces)
 
-ipr.tc("add-filter", "bpf", idx, ":1", fd=fi.fd,
-        name=fi.name, parent="ffff:", action="ok", classid=1, da=True)
+# interface = "ens4"
+#
+# ipr = IPRoute()
+# links = ipr.link_lookup(ifname=interface)
+# idx = links[0]
+#
+# try:
+#     ipr.tc("add", "egress", idx, ":1")
+# except:
+#     print("qdisc ingress already exists")
+#
+# fi = b.load_func("tc_ack", BPF.SCHED_CLS)
+#
+# ipr.tc("add-filter", "bpf", idx, ":1", fd=fi.fd,
+#         name=fi.name, parent="ffff:", action="ok", classid=1, da=True)
 
 b.trace_print()
 
