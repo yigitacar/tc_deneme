a2adcaa6a78c2213b2278c1e588186aa2cd46deb
yigitacar <yigit_flash3@live.com>
2024-12-09 10:31:46 +0300

    bug fix

--- tc_test/tc_test.h
+++ tc_test/tc_test.h
@@ -1,7 +1,7 @@
 #include <linux/icmp.h>
 #include <linux/if_ether.h>
 #include <linux/ip.h>
-#include <linux/bpf.h>
+//#include <linux/bpf.h>
 #include <linux/pkt_cls.h>
 #include <linux/in.h>
 #include <bpf/bpf_helpers.h>
