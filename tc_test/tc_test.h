681519fa556d1b1203a09f9ebd75e55494fa767e
yigitacar <yigit_flash3@live.com>
2024-12-11 14:34:09 +0300

    bug fix attempt

--- tc_test/tc_test.h
+++ tc_test/tc_test.h
@@ -8,7 +8,7 @@
 #include <linux/pkt_cls.h>
 #include <linux/in.h>
 #include <bpf/bpf_helpers.h>
-#include <bcc/proto.h>
+//#include <bcc/proto.h>
 
 static __always_inline unsigned short is_icmp_ping_request(void *data,
                                                            void *data_end) {
