b05e0275a185ceeb939bc15580a4183126dbcb57
yigitacar <yigit_flash3@live.com>
2024-12-18 11:28:17 +0300

    additional libraries included in redirect_egress.c

--- tc_cenv/redirect_egress.c
+++ tc_cenv/redirect_egress.c
@@ -1,3 +1,8 @@
+#include <linux/icmp.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/pkt_cls.h>
+#include <linux/in.h>
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
