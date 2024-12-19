46cc25ac85007785ce67442418b0138964e5784b
yigitacar <yigit_flash3@live.com>
2024-12-19 14:06:31 +0300

    bug fix attempt

--- tc_cenv/redirect_egress.c
+++ tc_cenv/redirect_egress.c
@@ -22,8 +22,9 @@ int tc_egress_multiplicate(struct __sk_buff *skb) {
 	
 //	#pragma unroll
     // Loop through all entries in the interface_map
-    for (key = 0; key < MAX_INTERFACE; key++) {
-        ifindex = bpf_map_lookup_elem(&interface_map, &key);
+    for (i = 0; i < MAX_INTERFACE; i++) {
+        key++;
+		ifindex = bpf_map_lookup_elem(&interface_map, &key);
 		if (!ifindex)
 			continue;
         if (ifindex && *ifindex > 0) {
