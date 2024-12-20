756b9f481b32947e8a948253a213a1f74f4191aa
yigitacar <yigit_flash3@live.com>
2024-12-20 16:53:58 +0300

    interface name extraction attempt

--- tc_test/tc_userspace.c
+++ tc_test/tc_userspace.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <unistd.h>
+#include <linux/if_link.h>
+
 #include "tc_kern.skel.h"
 
 #define BPF_PROG_PATH "tc_kern.o" // Path to compiled eBPF program
@@ -9,8 +17,19 @@ int main(int argc, char **argv)
 {
 	struct tc_kern *skel;
 	int err;
+	int count = 0;
 	
 	/* Get interface names */
+	char** card_data = getnics(&count);
+    for (int i = 0; i < count; i++) {
+        printf("%s\n", card_data[i]);
+    }
+    for (int i = 0; i < count; i++) { 
+        free(card_data[i]);
+    }
+    free(card_data);
+    return 0;	
+	
 	/* Update interface map */
 	
 	/* Create and open BPF application */
@@ -39,4 +58,45 @@ int main(int argc, char **argv)
 	/*  
 	tc_kern__destroy(skel);
 	*/
+}
+
+char** getnics(int* count)
+{
+    struct ifaddrs *ifaddr, *ifa;
+    int family, s;
+    char host[NI_MAXHOST];
+    char** details = NULL;
+    char* combined = NULL;
+    int index = 0;
+    
+
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr == NULL)
+            continue;
+
+        family = ifa->ifa_addr->sa_family;
+        if (family == AF_INET || family == AF_INET6) {
+            s = getnameinfo(ifa->ifa_addr,
+                    (family == AF_INET) ? sizeof(struct sockaddr_in) :
+                                            sizeof(struct sockaddr_in6),
+                    host, NI_MAXHOST,
+                    NULL, 0, NI_NUMERICHOST);
+            combined = malloc((strlen(ifa->ifa_name) + strlen(host) + 1) * sizeof(char*));
+            strcpy(combined, ifa->ifa_name);
+            strcat(combined, ":");
+            strcat(combined, host);
+            details = (char**) realloc(details, (index + 1) * sizeof(char*));
+            details[index] = malloc(strlen(combined) + 1);
+            strcpy(details[index++], combined);
+            free(combined);
+        } 
+    }
+    *count = index;
+    freeifaddrs(ifaddr);
+    return details;
 }
