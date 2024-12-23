383ee84115b501176bbbc04f64a52d53b675f43e
yigitacar <yigit_flash3@live.com>
2024-12-23 16:42:39 +0300

    attempt to open interface map

--- tc_test/tc_userspace.c
+++ tc_test/tc_userspace.c
@@ -47,9 +47,28 @@ int main(int argc, char **argv)
     //    goto cleanup;
     }	
 
+    // Find the map by its name
+    map = bpf_object__find_map_by_name(skel, "interface_map");
+    if (!map) {
+        fprintf(stderr, "Failed to find map 'interface_map'\n");
+        bpf_object__close(skel);
+        return 1;
+    }
+
+    // Get the map's file descriptor
+    map_fd = bpf_map__fd(map);
+    if (map_fd < 0) {
+        fprintf(stderr, "Failed to get file descriptor for map 'interface_map'\n");
+        bpf_object__close(skel);
+        return 1;
+    }
+
+    printf("Map file descriptor: %d\n", map_fd);
+
 	/* Update interface map */
-	key = 0;
-	bpf_map_update_elem(bpf_map__fd(skel.interface_map), &key, card_data[0], BPF_ANY);
+/*	key = 0;
+	bpf_map_update_elem(bpf_map__fd(skel->interface_map), &key, card_data[0], BPF_ANY);
+*/
 	
 	/* Attach tracepoint */
     err = tc_kern__attach(skel);
