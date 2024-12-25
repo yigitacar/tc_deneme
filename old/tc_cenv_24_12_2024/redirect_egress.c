f949797e6e4216a3546f3d6de39fe74af99523d9
yigitacar <yigit_flash3@live.com>
2024-12-25 09:45:11 +0300

    created test folder

---
+++ old/tc_cenv_24_12_2024/redirect_egress.c
@@ -0,0 +1,41 @@
+#include <linux/icmp.h>
+#include <linux/if_ether.h>
+#include <linux/ip.h>
+#include <linux/pkt_cls.h>
+#include <linux/in.h>
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+#define TC_ACT_OK 0
+#define TC_ACT_SHOT 1
+#define MAX_INTERFACE 10
+
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, __u32);
+	__uint(max_entries, MAX_INTERFACE);
+} interface_map SEC(".maps");
+
+SEC("classifier")
+int tc_egress_multiplicate(struct __sk_buff *skb) {
+    __u32 key = 0;
+    __u32 *ifindex;
+	
+    // Loop through all entries in the interface_map
+    for (int i = 0; i < MAX_INTERFACE; i++) {
+        ifindex = bpf_map_lookup_elem(&interface_map, &key);
+		if (!ifindex)
+			continue;
+        if (ifindex && *ifindex > 0) {
+            // Redirect the packet to the interface specified in the map
+            bpf_clone_redirect(skb, *ifindex, 0);
+        }
+		key++;
+    }
+
+    // Drop the original packet after cloning (optional)
+    return TC_ACT_OK;
+}
+
+char LICENSE[] SEC("license") = "GPL";
